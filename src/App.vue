c54b0a5b411762f4092b2ffea3ae323992a565fc
cmx <1497664458@qq.com>
2020-10-19 10:13:07 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <p>{{message}}</p>
+  </div>
+  <
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data(){
+    return{
+      message:'我是cli3'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+
+}
+
+</style>
---
+++ src/store/index.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+//1.安装插件
+Vue.use(vuex)
+
+
+//2.创建对象
+const store = new Vuex.Store({
+
+})
+
+//3.导出store独享
+export default store;
